4817c186ce33d2ece0068a43844ef8be4b91df41
animal <284649228@qq.com>
2024-08-11 15:52:59 +0800

    finish hw0b/src/Dessert

---
+++ hw0b/src/Dessert.java
@@ -0,0 +1,16 @@
+public class Dessert {
+    public int flavor;
+    public int price;
+    public static int numDesserts=0;
+    public Dessert(int f,int p){
+        this.flavor=f;
+        this.price=p;
+        numDesserts++;
+    }
+
+    public void printDessert(){
+        System.out.println(flavor+" "+price+" "+numDesserts);
+    }
+    public static void main(String[] args) {
+        System.out.println("I love dessert!");
+    }}
